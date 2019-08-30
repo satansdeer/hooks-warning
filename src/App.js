dcf32a37285461262ca464255c95c355e9ca7cd4
Maksim Ivanov <satansdeer@gmail.com>
2019-08-30 20:44:47 +0200

    Add practice code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import "./App.css";
+
+class ClassComponent extends React.Component {
+  _isMounted = false;
+  state = { data: null };
+
+  componentDidMount() {
+    this._isMounted = true;
+    axios
+      .get("https://hn.algolia.com/api/v1/search?query=react")
+      .then(result => {
+        // if (this._isMounted) {
+          this.setState({
+            data: result.data.hits
+          });
+        // }
+      });
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+    return <div>Component with data</div>;
+  }
+}
+
+// const Component = () => {
+//   const [, setData] = React.useState([]);
+
+//   React.useEffect(() => {
+//     let isMounted = true;
+//     axios
+//       .get("https://hn.algolia.com/api/v1/search?query=react")
+//       .then(result => {
+//         if (isMounted) {
+//           setData({
+//             data: result.data.hits
+//           });
+//         }
+//       });
+//     return () => (isMounted = false);
+//   }, []);
+
+//   return <div>Functional component</div>;
+// };
 
 function App() {
+  const [amount, setAmount] = useState(1);
+
+  useEffect(() => {
+    setInterval(() => {
+      setAmount(n => n + 1);
+    }, 5);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {[...Array(amount).keys()].map(id =>
+        id < amount - 10 ? null : <ClassComponent key={id} />
+      )}
     </div>
+    // <div className="App">
+    //   {[...Array(amount).keys()].map(id => (id < amount - 10 ? null : <div key={id}>Component</div>))}
+    // </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// var x = [];
+
+// function grow() {
+//   x.push(new Array(1000000).join('x'));
+// }
+
+// document.getElementById('grow').addEventListener('click', grow);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
